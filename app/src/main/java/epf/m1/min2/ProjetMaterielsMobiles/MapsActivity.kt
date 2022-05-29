c97d18199948649d76061e3ade45d6bcdfd434df
Anatole Durré <anatole.durre@hotmail.com>
2022-05-29 19:46:45 +0200

    commit anatole carte paris

--- app/src/main/java/epf/m1/min2/ProjetMaterielsMobiles/MapsActivity.kt
+++ app/src/main/java/epf/m1/min2/ProjetMaterielsMobiles/MapsActivity.kt
@@ -1,12 +1,8 @@
 package epf.m1.min2.ProjetMaterielsMobiles
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import epf.m1.min2.ProjetMaterielsMobiles.databinding.ActivityMapsBinding
@@ -41,8 +37,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val paris = LatLng(48.52, 2.19)
+        mMap.addMarker(MarkerOptions().position(paris).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(paris))
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(5F))
+
+// Sets the map type to be "hybrid"
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+
     }
 }
